27fa893662f7c183a92d8f5e71d22ec08f2a71cf
Your Name <joao.cedan.oak@gmail.com>
2025-08-31 22:16:22 -0300

    feat - add migration

--- app/User/domain/entity/user.ts
+++ app/User/domain/entity/user.ts
@@ -2,5 +2,9 @@ export interface User {
   id: string;
   name: string;
   email: string;
+  password: string;
   age: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date | null;
 }
---
+++ migrations/20250901011105-tabela_user.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable("users", {
+      id: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
